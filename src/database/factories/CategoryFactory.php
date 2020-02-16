fcd09421316ad52e63f68edd849b652c2c3d1150
Osman Aras <osman.aras@mynetgroup.com>
2020-02-16 16:56:42 +0300

    Song, Category, Favorite -> factory ve seeder oluşturuldu

---
+++ src/database/factories/CategoryFactory.php
@@ -0,0 +1,13 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model;
+use Faker\Generator as Faker;
+
+$factory->define(\App\Category::class, function (Faker $faker) {
+    return [
+        'name' => $faker->realText(10),
+        'image' => '/asset/image/'.$faker->randomDigit
+    ];
+});
